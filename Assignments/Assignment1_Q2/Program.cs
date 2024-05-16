af701896acaaa7194fcd5edad06b515008dfd6b4
D1-Nishank-83725 <nishank.kose77@gmail.com>
2024-05-16 14:53:27 +0530

    Changed Folder name to Assignments

---
+++ Assignments/Assignment1_Q2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Assignment1_Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = Convert.ToInt32(args[0]);
+            int num2 = Convert.ToInt32(args[1]);
+
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("1: Press '+' for Addition ");
+                Console.WriteLine("2: Press '-' for Subtraction ");
+                Console.WriteLine("3: Press '*' for Multiplication ");
+                Console.WriteLine("4: Press '/' for Division ");
+                Console.WriteLine();
+                String cce = Console.ReadLine();
+
+                switch (cce)
+                {
+                    case "+":
+                        Console.WriteLine("The Addition of " + num1 + " + " + num2 + " = " + (num1 + num2));
+                        break;
+
+                    case "-":
+                        Console.WriteLine("The Subtraction of " + num1 + " - " + num2 + " = " + (num1 - num2));
+                        break;
+
+                    case "*":
+                        Console.WriteLine("The Multiplication of " + num1 + " * " + num2 + " = " + (num1 * num2));
+                        break;
+
+                    case "/":
+                        Console.WriteLine("The Division of " + num1 + " / " + num2 + " = " + (num1 / num2));
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Choice!");
+                        break;
+
+                }
+
+
+                Console.WriteLine("Press 'e' to Exit, any other key to Continue: ");
+                String exitornot = Console.ReadLine();
+                if (exitornot == "e")
+                {
+                    Console.WriteLine("Thank you for using the Programme");
+                    Environment.Exit(0);
+                }
+
+
+
+            } while (true);
+
+
+        }
+    }
+}

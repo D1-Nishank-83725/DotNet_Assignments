2361e78793054e791a48cddd7f3db494e411dea7
D1-Nishank-83725 <nishank.kose77@gmail.com>
2024-05-20 18:35:56 +0530

    Question 6 Done

---
+++ Assignments/Question6/Program.cs
@@ -0,0 +1,108 @@
+﻿namespace Question6
+{
+    internal class Program
+    {
+        class Date
+        {
+            private int _day;
+            private int _month;
+            private int _year;
+
+            public Date(){
+                _day = 0;
+                _month = 0;
+                _year = 0;
+            }
+
+            public Date(int day, int month, int year)
+            {
+                Day = day;
+                Month = month;
+                Year = year;
+            }
+
+            public int Year
+            {
+                get { return _year; }
+                set { _year = value; }
+            }
+
+
+            public int Month
+            {
+                get { return _month; }
+                set { _month = value; }
+            }
+
+
+            public int Day
+            {
+                get { return _day; }
+                set { _day = value; }
+            }
+
+            public void AcceptDate()
+            {
+                Console.WriteLine("Enter Day : ");
+                _day = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Month : ");
+                _month = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Day : ");
+                _year = Convert.ToInt32(Console.ReadLine());
+            }
+
+            public void PrintDate()
+            {
+                Console.WriteLine("Date: " + _day + "/" + _month + "/" + _year);
+            }
+
+            public bool IsValid()
+            {
+                if ((_day >= 01 && _day <= 31) && (_month >= 01 && _month <= 12) && (_year >= 1900 && _day <= 3000))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public string ToString()
+            {
+                return ("Date: " + _day + "/" + _month + "/" + _year);
+            }
+
+
+            public static Date operator - (Date d1, Date d2)
+            {
+                Date d3 = new Date();
+                d3.Day = Math.Abs(d1.Day - d2.Day);
+                d3.Month = Math.Abs(d1.Month - d2.Month);
+                d3.Year = Math.Abs(d1.Year - d2.Year);
+
+                return d3;
+            }
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            Date d1 = new Date();
+            d1.AcceptDate();
+            Date d2 = new Date();
+            d2.AcceptDate();
+
+            Console.WriteLine();
+            d1.PrintDate();
+            Console.WriteLine();
+            d2.PrintDate();
+
+            Date d3 = d1 - d2;
+            Console.WriteLine("Difference between Dates in Number of Years is: " + d3.Year);
+            
+
+        }
+    }
+}
